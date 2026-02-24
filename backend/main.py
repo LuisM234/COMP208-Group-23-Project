4013bd263417699eff0b17f6e93a450fc7343404
jarad <j@jar.rip>
2026-02-24 16:07:22 +0000

    send to docs when root of api is accessed

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
+from fastapi.responses import ORJSONResponse, RedirectResponse
 from routes.ai import ai_route
 from routes.decks import decks_route
 
@@ -9,6 +9,11 @@ app = FastAPI(
     # speeds up responses from the API
     default_response_class=ORJSONResponse,
 )
+
+# send to docs if root is accessed
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/docs")
 
 app.include_router(ai_route)
 app.include_router(decks_route)
