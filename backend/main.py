cedaf01b531969e88575538f5bf544fc724d46d8
Luis M <190517802+LuisM234@users.noreply.github.com>
2026-04-19 23:44:13 +0100

    IMPORTANT: add frontend url to main.py
    allows me to connect our deployed frontend to our deployed backend.

--- backend/main.py
+++ backend/main.py
@@ -35,6 +35,7 @@ app.add_middleware(
         "http://localhost:8080",
         "http://127.0.0.1:8080",
         "null",  # allows file:// origin (browsers send Origin: null for local files)
+        "https://startling-chaja-8dedf0.netlify.app", # production frontend
     ],
     allow_credentials=True,
     allow_methods=["*"],
